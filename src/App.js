85471c4a7f9c434d055ec6d578d6dae203c9da9a
Hamza HArmouche <58759442+harmouche@users.noreply.github.com>
2021-03-16 00:24:39 -0400

    added components and initial state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./components/Header";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
-    <div>
       <div className="container">
         <div className="app-wrapper">
           <div>
             <Header />
           </div>
+          <div>
+            <Form 
+              input = {input}
+              setInput = {setInput}
+              todos = {todos}
+              setTodos = {setTodos}   
+            />                     
+          </div>
+          <div>
+            <TodoList todos={todos} setTodos={setTodos} />
+          </div>
         </div>
       </div>
-    </div>
   );
 }
 
